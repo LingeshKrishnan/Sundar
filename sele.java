fc3210f453e9e6466a7ff94089aafbb3bfaed12a
LingeshKrishnan <sundhar06@gmail.com>
2018-05-13 01:26:42 -0700

    create sele.java
    example for sele java

---
+++ sele.java
@@ -0,0 +1,21 @@
+import org.selenium.*
+
+//for a webapp login
+
+public class sele {
+
+public static void main(String args[]) {
+
+webdriver driver = new ChromeDriver();
+driver.get("url"); //url to test
+driver.manage().window().maximize(); //maximize the chrome window
+driver.findElmentById.("//locator").sendKeys("to enter"); //id locator for text box
+driver.findElemnetBy*("//locator").sendKeys("//pwd");
+driver.findElmenetBY**("//locator fot element or button").Click();//button click
+
+
+
+}
+
+
+}
